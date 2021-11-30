bec6037941f4c9c667817a56cf8817c76fa61b1d
Anatoly <podyapolskiyaa@gmail.com>
2021-11-30 20:07:22 +0300

    Add Heroes table

---
+++ app/models/hero.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class Hero(db.Model):
+    __tablename__ = 'Heroes'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(25), unique=True, nullable=False)
+    attribute = db.Column(db.String(3), nullable=False)
+    attack_type = db.Column(db.String(6), nullable=False)
+    roles = db.Column(db.String(50))
+
+    def __repr__(self) -> str:
+        return f'<object Hero(\n\tid: {self.id},\n\tname: {self.name}\n)>'
