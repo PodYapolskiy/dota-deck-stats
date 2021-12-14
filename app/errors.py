6a41819c5b8b817460d985f28a1f87aadaeb3f18
Anatoly <podyapolskiyaa@gmail.com>
2021-12-14 16:02:19 +0300

    Add error handlers

--- app/errors.py
+++ app/errors.py
@@ -9,4 +9,4 @@ def not_found_error(error):
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return render_template('error/500.html'), 500
+    return render_template('errors/500.html'), 500
